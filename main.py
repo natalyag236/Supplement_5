997c08cbd1f6fa07471055561f58392189a89af4
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-25 22:50:45 -0500

    doc: add documentation to calculate square root

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import math
 def square_root(number):
+    """Calculate the sqaure root of a given number.
+
+    Args:
+        number (float): the number to calculate the sqaure root. Must be non-negative.
+
+    Raises:
+        ValueError: If the input number is negative
+
+    Returns:
+        float: the sqaure root of the input number
+    """
     if number < 0:
         raise ValueError("Cannot calculate the square root of a negative number")
     return math.sqrt(number)

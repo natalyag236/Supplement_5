c17650f04cf06a083e8da3f79319341bb565b883
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-25 22:48:15 -0500

    feat:implement sqaure root calcuation function

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import math
+def square_root(number):
+    if number < 0:
+        raise ValueError("Cannot calculate the square root of a negative number")
+    return math.sqrt(number)
+
 def test_square_root_():
     assert square_root(9) == 3
